0fff6090b5fe82e66d2688163d1189b0e54a9484
Seyi Adekoya <seyi.adekoya@andela.com>
2016-04-12 23:07:46 +0100

    much more cleaniness

--- src/inverted-index.js
+++ src/inverted-index.js
@@ -1,34 +1,25 @@
 'use strict';
 
+//The InvertedIndex object takes JSON files and indexes them
 var InvertedIndex = function() {
   var that = this;
 
   this.index = {};
-  this.jsonArray = [];
 
+  /**
+   * Returns index property of the Object
+   * @return Object 
+   */
   this.getIndex = function () {
     return that.index;
   };
 
-  this.createIndex = function (filepath) {
-
-    var populateIndex = function (json) {
-      json.forEach(function (book, location) {
-        var words = book.title + ' ' + book.text;
-        words = that.tokenize(words);
-
-        words.forEach(function (word) {
-          var normalizedWord = that.normalize(word);
-
-          if (! that.index.hasOwnProperty(normalizedWord) ) {
-            that.index[normalizedWord] = [location];
-
-          } else if(that.index[normalizedWord].indexOf(location) < 0) {
-            that.index[normalizedWord].push(location);
-          }
-        });
-      });
-    };
+  /**
+   * Performs a synchronous request for the json
+   * @param  {String} filepath String representing the path of the file
+   * @return {Object}          Parsed JSON Response of the JSON file
+   */
+  this.readJsonFile = function (filepath) {
     var response;
     var xhr = new XMLHttpRequest();
 
@@ -38,23 +29,53 @@ var InvertedIndex = function() {
       var data = JSON.parse(xhr.responseText);
       response = data;
       that.jsonArray = data;
-      populateIndex(data);
     }
 
     return response;
   };
 
+  /**
+   * Creates the index property on the object
+   * @param  {String} filepath String representing the filepath
+   * @return {Object}          Parsed JSON Response from the readJsonFile method
+   */
+  this.createIndex = function (filepath) {
+
+    var json = that.readJsonFile(filepath);
+    
+    json.forEach(function (book, location) {
+      var words = book.title + ' ' + book.text;
+      words = that.tokenize(words);
+
+      words.forEach(function (word) {
+        var normalizedWord = that.normalize(word);
+
+        if (! that.index.hasOwnProperty(normalizedWord) ) {
+          that.index[normalizedWord] = [location];
+
+        } else if(that.index[normalizedWord].indexOf(location) < 0) {
+          that.index[normalizedWord].push(location);
+        }
+      });
+    });
+
+    return json;
+  };
+
+  /**
+   * Performs search on index property
+   * @param  {mixed} query query can be an array or a string
+   * @return {Array}       An array containing search results
+   */
   this.searchIndex = function (query) {
     var result = [];
-    //var query = Array.prototype.slice.call(arguments);
 
     var performSearch = function (searchWords) {
       for (var i = 0; i < searchWords.length; i++) {
         var arg = searchWords[i];
         arg = that.normalize(arg);
-        if (typeof that.index[arg] === 'undefined') {
 
-        } else {
+        if (typeof that.index[arg] !== 'undefined') {
           result.push(that.index[arg]);
         }
       }
@@ -70,10 +91,20 @@ var InvertedIndex = function() {
     return result;
   };
 
+  /**
+   * Splits the words in the documents or JSON objects into an array
+   * @param  {String} words Sentence or strings in documents
+   * @return {Array}       Splitted words in an array
+   */
   this.tokenize = function (words) {
     return words.split(' ');
   };
 
+  /**
+   * Turns the tokens to lowercase and removes special characters
+   * @param  {String} word The tokenized string is taken in here
+   * @return {String}      The cleaned up string
+   */
   this.normalize = function (word) {
     word = word.replace(/[,";:?!@#$%(^)&*()_+|.><{}±]/, '');
     return word.toLowerCase();
